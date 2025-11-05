4ef8b88fd90b016581e20268b3db4041142ec4ff
Rama Kishore Reddy Singam <singamrk@Ramas-iMac.local>
2025-11-04 19:35:39 -0500

    Web app along with Dockerfile

---
+++ app/app.py
@@ -0,0 +1,14 @@
+from import flask, jsonify
+
+app = Flask(__name__)
+
+@app.get("/")
+def root():
+    return "Welcome to simple webapp"
+
+@app.get("/healthz")
+def healthz():
+    return jsonify(status:ok)
+
+if __name__ == "__main__":
+    app.run(host:"0.0.0.0", port: 8080)
